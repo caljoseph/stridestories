f000e8e9f2e03aba7f81b661a50c93fb7834c2a8
caljoseph <therealcalebbradshaw@gmail.com>
2024-03-21 12:38:59 -0600

    add api calls for login and run functionality

--- index.js
+++ index.js
@@ -28,21 +28,94 @@ app.set('trust proxy', true);
 const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-//http requests will use apiRouter ex apiRouter.get('/path', (_req, res) => { res.send(scores); });
-apiRouter.get('/runs', async (_req, res) => {
-  res.send(runRecords);
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.username)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.username, req.body.password);
+
+    // Set the cookie
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
 });
 
-apiRouter.post('/run', (req, res) => {
-  runRecords = addRecord(req.body, runRecords)
-  res.send(runRecords);
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.username);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
 });
 
-function addRecord(run, runRecords) {
-  runRecords.push(run);
-  runRecords.sort((a, b) => new Date(a.date) - new Date(b.date));
-  return runRecords;
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// GetUser returns information about a user
+apiRouter.get('/user/:username', async (req, res) => {
+  const user = await DB.getUser(req.params.username);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ username: user.username, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+// GetRuns
+secureApiRouter.get('/runs', async (req, res) => {
+  const runs = await DB.getRuns();
+  res.send(runs);
+});
+
+// SubmitRuns
+secureApiRouter.post('/run', async (req, res) => {
+  const run = { ...req.body, ip: req.ip };
+  await DB.addRun(run);
+  const runs = await DB.getRuns();
+  res.send(runs);
+});
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
 }
+
 
 
 
