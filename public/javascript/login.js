dc7b128148218db80150c17e0b96df9fb8f25080
caljoseph <therealcalebbradshaw@gmail.com>
2024-03-21 14:44:37 -0600

    implement login and register

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,4 @@
-function validateAndLogin() {
+async function validateAndLogin() {
   const userNameInput = document.querySelector("#name");
   const passwordInput = document.querySelector("#password");
 
@@ -19,10 +19,91 @@ function validateAndLogin() {
   }
 
   // If validation passes, proceed with login
-  localStorage.setItem("username", userNameInput.value.trim());
-  localStorage.setItem("password", passwordInput.value.trim());
+  const body = {
+    "username" : userNameInput.value,
+    "password" : passwordInput.value
+  };
 
-  // Redirect or perform other actions as needed
-  window.location.href = "logged-in.html";
+  if (await login(body)) {
+    window.location.href = "logged-in.html";
+    return;
+  } else {
+    console.log("login failed")
+
+  }
+
 }
 
+async function validateAndRegister() {
+  const userNameInput = document.querySelector("#name");
+  const passwordInput = document.querySelector("#password");
+
+
+  userNameInput.classList.remove("is-invalid");
+  passwordInput.classList.remove("is-invalid");
+
+  if (!userNameInput.value) {
+    userNameInput.classList.add("is-invalid");
+    document.getElementById("username-error").innerText = "Username required";
+    return;
+  }
+
+  if (!passwordInput.value) {
+    passwordInput.classList.add("is-invalid");
+    document.getElementById("password-error").innerText = "Password required";
+    return;
+  }
+
+  // If validation passes, proceed with register
+  const body = {
+    "username" : userNameInput.value,
+    "password" : passwordInput.value
+  };
+
+  if (await register(body)) {
+    window.location.href = "logged-in.html";
+    return;
+  } else {
+    console.log("register failed")
+  }
+
+}
+
+
+async function login(body) {
+
+  try {
+      await sendPostRequest("/api/auth/login", body);
+      console.log("Logged in successfully")
+  } catch (error) {
+      console.error("Unable to login", error.message);
+      return false;
+  }
+
+  return true;
+}
+async function register(body) {
+
+  try {
+      await sendPostRequest("/api/auth/create", body);
+      console.log("Registered successfully")
+  } catch (error) {
+      console.error("Unable to register", error.message);
+      return false;
+  }
+
+  return true;
+}
+
+async function sendPostRequest(url, data) {
+  const response = await fetch(url, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+  });
+  if (!response.ok) {
+      throw new Error(`Post request failed: ${response.statusText}`);
+  }
+}
