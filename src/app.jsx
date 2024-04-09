5351ada4758379df1c9204ec19afc06374783d38
caljoseph <therealcalebbradshaw@gmail.com>
2024-04-09 14:18:15 -0600

    transition header and footer to react

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <div className='body bg-dark text-light'>
+        <header>
+            <h1>Stridestories</h1>
+            <nav>
+            <ul className="menu-bar">
+                <li id="leaderboard"><a href="leaderboard.html">Leaderboard</a></li>
+                <li id="about"><a href="about.html">About</a></li>
+            </ul>
+            </nav>
+        </header>
+        <main>The real stuff goes here</main>
+        <footer>
+            <span className="text-reset">Caleb Bradshaw</span>
+            <br />
+            <a href="https://github.com/caljoseph/startup">GitHub</a>
+        </footer>
+    </div>
+
+
+
+
+
+    
+  );
+  
+
 }
